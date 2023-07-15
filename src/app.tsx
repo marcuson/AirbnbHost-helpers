765ea66aa1ed28aa74e15698f48ee14e2403fe9a
Leonardo Marcucci <l.marcucci@sintraconsulting.eu>
2023-07-15 19:15:59 +0200

    fix: Fix pdfjs-lib worker loading.

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import globalCss from './style.css';
 console.info('Loading Airbnb host helper');
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-  '//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.5.141/pdf.worker.min.js';
+  '//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.8.162/pdf.worker.min.js';
 
 document.head.append(VM.m(<style>{globalCss}</style>));
 moduleLoaderInit();
